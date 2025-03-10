e7ef7b3364a84b810c4c7420a90e0c7b39a98d8a
Ochieng' Meshack Otieno <146129953+Meshack12756@users.noreply.github.com>
2025-03-10 10:16:12 -0700

    Update login.php

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
 if (isset($_GET["error"])) {
 
     if ($_GET["error"] == "emptyinput") {
-        echo"<p>Fill in all fields!</p>";
+        echo"<p>Please fill in all fields!</p>";
     }
 
     elseif ($_GET["error"] == "wronglogin") {
